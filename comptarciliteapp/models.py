5bf7ba3cf8781e41abdb2c49d72755d30a81234a
Tristan Seuret <tristan.seuret@he-arc.ch>
2020-03-03 11:03:14 +0100

    Added Models

--- comptarciliteapp/models.py
+++ comptarciliteapp/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+# Account
+class Account(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.TextField()
+    members = models.ManyToManyField('auth_user', through='Membership')
+
+# Transactions
+class Transaction(models.Model):
+    timestamp = models.DateTimeField()
+    amount = models.DecimalField(max_digits=16, decimal_places=2)
+    description = models.TextField()
+    id_account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    id_user = models.ForeignKey('auth_user', on_delete=models.SET_NULL, null=True)
+    #id_cat = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+
+#class Category(models.Model):
+    #name = models.CharField(max_length=30)
+    #description = models.TextField()
+
+class Membership(models.Model):
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+    user = models.ForeignKey('auth_user', on_delete=models.CASCADE)
+    is_owner = models.BooleanField()
+
