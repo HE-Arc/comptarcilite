d54d061668b77d84892ba01ba9c4e95772bda768
Tristan Seuret <tristan.seuret@he-arc.ch>
2020-03-17 10:10:16 +0100

    Added index template

--- comptarciliteapp/views.py
+++ comptarciliteapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import generic, View
+from django.urls import reverse_lazy
+
 
 # Create your views here.
+def index(request):
+    context = {}
+    return render(request, 'index.html', context)
