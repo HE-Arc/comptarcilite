12b0cdb4de95325da349beb36c4963e3a6771ace
Julien Chappuis <julien.chappuis@he-arc.ch>
2020-03-24 13:55:27 +0100

    modification urls.py

--- comptarciliteapp/urls.py
+++ comptarciliteapp/urls.py
@@ -7,7 +7,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     #path('login/',TemplateView.as_view(template_name="registration/login.html"),name='login'),
     path('admin/', admin.site.urls),
-    path('auth/', include('django.contrib.auth.urls')),
     path('auth/login', auth_views.LoginView.as_view(redirect_authenticated_user=True), name='login'),
     path('create_account', views.AccountCreateView.as_view(), name='create_account'),
     path('auth/', auth_views.LoginView.as_view(redirect_authenticated_user=True)),
@@ -19,5 +18,6 @@ urlpatterns = [
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    
     path('', views.index, name='index'),
 ]
