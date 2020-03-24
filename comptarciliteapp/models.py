d61140fac5c87fd521e2cc73639c83d04030ea12
Téo Schaffner <teo.schaffner@he-arc.ch>
2020-03-24 13:53:48 +0100

    activé les modèles dans le panneau admin

--- comptarciliteapp/models.py
+++ comptarciliteapp/models.py
@@ -1,11 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Account
 class Account(models.Model):
     name = models.CharField(max_length=30)
     description = models.TextField()
     members = models.ManyToManyField(User, through='Membership')
+
+    def __str__(self):
+        return self.name
+
 
 # Transactions
 class Transaction(models.Model):
@@ -16,12 +21,15 @@ class Transaction(models.Model):
     id_user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     #id_cat = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
 
+    def __str__(self):
+        return str(self.timestamp) + " (" + str(self.amount) + ")"
+
 #class Category(models.Model):
     #name = models.CharField(max_length=30)
     #description = models.TextField()
+
 
 class Membership(models.Model):
     account = models.ForeignKey(Account, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     is_owner = models.BooleanField()
-
--- comptarciliteapp/urls.py
+++ comptarciliteapp/urls.py
@@ -6,7 +6,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     #path('login/',TemplateView.as_view(template_name="registration/login.html"),name='login'),
-    #path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('auth/', include('django.contrib.auth.urls')),
     path('auth/login', auth_views.LoginView.as_view(redirect_authenticated_user=True), name='login'),
     path('create_account', views.AccountCreateView.as_view(), name='create_account'),
