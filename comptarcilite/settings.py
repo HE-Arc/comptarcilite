2c73d695424ed68de85d808e04a42f2fcb352026
Julien Chappuis <julien.chappuis@he-arc.ch>
2020-04-22 18:00:26 +0200

    fix config

--- comptarcilite/settings.py
+++ comptarcilite/settings.py
@@ -137,10 +137,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+
+STATIC_ROOT = f'{BASE_DIR}/static'
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, "static"),
+#]
+
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 ]
+
+
 
 #EMAIL
 from decouple import config
